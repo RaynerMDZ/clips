7209a2da60f336122b0732cfc762ed973ce888a3
rayner <raynermendezg@gmail.com>
2022-06-14 18:44:39 -0400

    Add logic to toggle between tabs in the modal component.

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,36 @@
+import {AfterContentInit, Component, ContentChildren, OnInit, QueryList} from '@angular/core';
+import {TabComponent} from "../tab/tab.component";
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements OnInit, AfterContentInit {
+
+  @ContentChildren(TabComponent)
+  tabs?: QueryList<TabComponent> = new QueryList<TabComponent>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterContentInit() {
+    const activeTabs = this.tabs?.filter(
+      tab => tab.active
+    );
+
+    if (!activeTabs || activeTabs.length === 0) this.selectTab(this.tabs!.first);
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => tab.active = false);
+    tab.active = true;
+
+    // prevents the default behavior.
+    return false;
+  }
+
+}
